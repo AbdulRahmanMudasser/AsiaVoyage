d90cf083e501aeef5325df68d365dbfaa8e5d711
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2024-08-27 11:36:36 +0500

    database setup
    - Created Tour model
    - Applied migrations

--- asiavoyage/models.py
+++ asiavoyage/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Tour(models.Model):
+    # orgin country
+    origin_conutry = models.CharField(max_length=64)
+    
+    # destination country
+    destination_country = models.CharField(max_length=64)
+    
+    # number of nights
+    number_of_nights = models.IntegerField()
+    
+    # price of tour
+    price = models.IntegerField()
