9eb095f956b5795f0749c99f6347433b320422be
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2024-08-29 11:17:52 +0500

    adding and displaying data records
    - Adding data records in table
    - Displaying data on html page
    - Rendering index.html
    - Configuring view.html file

--- asiavoyage/views.py
+++ asiavoyage/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Tour
+
 def index(request):
-    return HttpResponse("Asia Voyage")
+    tours = Tour.objects.all()
+    
+    context = {"tours": tours}
+    
+    return render(request, "tours/index.html", context)
 
 def welcome(request):
     return HttpResponse("Welcome to Asia Voyage")
